479775f72532266231fa1795beaca60891b7be56
ysfsalman <yusufsalman@hotmail.co.id>
2020-07-28 13:45:45 +0700

    FP.4 implement method Send

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -18,6 +18,9 @@ void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::lock_guard<std::mutex> lock(_mtx);
+    _messages.emplace_back(std::move(msg)); 
+    _cond.notify_one();
 }
 
 
@@ -74,7 +77,7 @@ void TrafficLight::cycleThroughPhases()
         
         // send update message to queue
         //futures.emplace_back(std::async(std::launch::async, &MessageQueue<TrafficLightPhase>::send, queue, std::move(_currentPhase)));
-        //_queue->send(std::move(_currentPhase));
+        _queue->send(std::move(_currentPhase));
         
         
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -54,7 +54,7 @@ private:
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
     // send in conjunction with move semantics.
-    //std::shared_ptr<MessageQueue<TrafficLightPhase>> _queue(new MessageQueue<TrafficLightPhase>);
+    std::shared_ptr<MessageQueue<TrafficLightPhase>> _queue{new MessageQueue<TrafficLightPhase>};
     std::condition_variable _condition;
     std::mutex _mutex;
 };
